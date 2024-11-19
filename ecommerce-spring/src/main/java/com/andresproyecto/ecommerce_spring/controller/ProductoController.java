87cae134816aff589fe38f26464bbf42770f2807
andres <andres-j7@hotmail.com>
2024-11-18 23:55:01 -0500

    Funcionalidad para editar un producto

--- ecommerce-spring/src/main/java/com/andresproyecto/ecommerce_spring/controller/ProductoController.java
+++ ecommerce-spring/src/main/java/com/andresproyecto/ecommerce_spring/controller/ProductoController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
 
 
 @Controller
@@ -35,6 +37,21 @@ public class ProductoController {
         Usuario u= new Usuario(1,"", "","","","","","");
         producto.setUsuario(u);
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto>optionalProducto=productoService.get(id);
+        producto = optionalProducto.get();
+        LOGGER.info("Este es el  producto buscado {}", producto);
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
         return "redirect:/productos";
     }
 }
