3dbd8565d819ab84ac35ec921485ea718216e1a4
andres <andres-j7@hotmail.com>
2024-11-23 23:27:47 -0500

    En lace carrito

--- ecommerce-spring/src/main/java/com/andresproyecto/ecommerce_spring/controller/HomeController.java
+++ ecommerce-spring/src/main/java/com/andresproyecto/ecommerce_spring/controller/HomeController.java
@@ -58,7 +58,13 @@ public class HomeController {
         detalleOrden.setNombre(producto.getNombre());
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
-        detalles.add(detalleOrden);
+        //validar que el carrito no se añada dos veces
+        Integer idProducto=producto.getId();
+        boolean ingresado=detalles.stream().anyMatch(p ->p.getProducto().getId()==idProducto);
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
+
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
         orden.setTotal(sumaTotal);
         model.addAttribute("cart", detalles);
@@ -86,5 +92,11 @@ public class HomeController {
 
         return"usuario/carrito";
 
+    }
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
     }
 }
